573470066d57c06e67c129b02e9c65e8a5610df9
Rohit Dhatrak <57338980+rohit436@users.noreply.github.com>
2020-06-22 15:22:33 +0530

    added script for outline

--- javascript/calculator.js
+++ javascript/calculator.js
@@ -4,15 +4,16 @@ let operand;
 let total = 0;
 let counter = 0;
 
-const inpVal = document.querySelector(".buttons").addEventListener("click",function(event) {
-    determineInput(event.target.innerText);
-});
+const inpVal = document
+    .querySelector(".buttons")
+    .addEventListener("click", function (event) {
+        determineInput(event.target.innerText);
+    });
 
 function determineInput(value) {
     if (isNaN(parseInt(value))) {
         determineOperator(value);
-    }
-    else {
+    } else {
         determineNumber(value);
     }
 }
@@ -23,39 +24,28 @@ function determineNumber(value) {
 }
 
 function determineOperator(value) {
-    
     if (value === ".") {
         determineNumber(value);
-    }
-
-    else if (value === "←") {
-        buffer = buffer.substring(0,buffer.length - 1);
+    } else if (value === "←") {
+        buffer = buffer.substring(0, buffer.length - 1);
         display(buffer);
-    }
-
-    else if (value == "±") {
+    } else if (value == "±") {
         buffer = "-";
         display(buffer);
-    }
-
-    else if (value === "C") {
+    } else if (value === "C") {
         counter = 0;
         total = 0;
         buffer = "";
         display(total);
-    }
-
-    else if (value === "=") {
+    } else if (value === "=") {
         operand = Number(buffer);
         counter += 1;
         calculateValue(operator);
-        buffer = total.toString();    // to be able to continue operating on the answer
+        buffer = total.toString(); // to be able to continue operating on the answer
         counter = 0;
-    }
-
-    else {
+    } else {
         display(value);
-        operand = Number(buffer);    // after getting a operator we know that input is complete 
+        operand = Number(buffer); // after getting a operator we know that input is complete
         counter += 1;
         calculateValue(value);
         buffer = "";
@@ -63,19 +53,15 @@ function determineOperator(value) {
 }
 
 function calculateValue(value) {
-
     if (counter === 1) {
         total = operand;
-    }
-    
-    else {
-        
+    } else {
         if (operator === "+") {
             total += operand;
-            if (total % 1 === 0) {          // to check for the presence of decimal
-                display(total);    
-            }
-            else {
+            if (total % 1 === 0) {
+                // to check for the presence of decimal
+                display(total);
+            } else {
                 display(total.toFixed(2));
             }
         }
@@ -83,9 +69,8 @@ function calculateValue(value) {
         if (operator === "-") {
             total -= operand;
             if (total % 1 === 0) {
-                display(total);    
-            }
-            else {
+                display(total);
+            } else {
                 display(total.toFixed(2));
             }
         }
@@ -93,9 +78,8 @@ function calculateValue(value) {
         if (operator === "×") {
             total *= operand;
             if (total % 1 === 0) {
-                display(total);    
-            }
-            else {
+                display(total);
+            } else {
                 display(total.toFixed(2));
             }
         }
@@ -103,9 +87,8 @@ function calculateValue(value) {
         if (operator === "÷") {
             total /= operand;
             if (total % 1 === 0) {
-                display(total);    
-            }
-            else {
+                display(total);
+            } else {
                 display(total.toFixed(2));
             }
         }
@@ -114,6 +97,35 @@ function calculateValue(value) {
 }
 
 function display(displayVal) {
-    const disp = document.querySelector('.display');
+    const disp = document.querySelector(".display");
     disp.innerText = displayVal;
 }
+
+(function (d) {
+    var style_element = d.createElement("STYLE"),
+        dom_events = "addEventListener" in d,
+        add_event_listener = function (type, callback) {
+            // Basic cross-browser event handling
+            if (dom_events) {
+                d.addEventListener(type, callback);
+            } else {
+                d.attachEvent("on" + type, callback);
+            }
+        },
+        set_css = function (css_text) {
+            // Handle setting of <style> element contents in IE8
+            !!style_element.styleSheet
+                ? (style_element.styleSheet.cssText = css_text)
+                : (style_element.innerHTML = css_text);
+        };
+    d.getElementsByTagName("HEAD")[0].appendChild(style_element);
+
+    // Using mousedown instead of mouseover, so that previously focused elements don't lose focus ring on mouse move
+    add_event_listener("mousedown", function () {
+        set_css(":focus{outline:0}::-moz-focus-inner{border:0;}");
+    });
+
+    add_event_listener("keydown", function () {
+        set_css("");
+    });
+})(document);
